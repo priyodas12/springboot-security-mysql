c8ab2f448652c14a2c4f167aa87f11c933644f77
Priyobrato Das <priyodas12@gmail.com>
2020-08-13 23:45:15 +0530

    Initial commit

---
+++ src/main/java/io/springbootlab/springbootsecuritymysql/SpringbootSecurityMysqlApplication.java
@@ -0,0 +1,13 @@
+package io.springbootlab.springbootsecuritymysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/springbootlab/springbootsecuritymysql/SpringbootSecurityMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbootlab.springbootsecuritymysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
