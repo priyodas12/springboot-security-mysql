82ec2a530b9ae4fa0b5eb6ee2d0d7f9d087d67e5
Priyobrato Das <priyodas12@gmail.com>
2020-08-14 00:37:11 +0530

    jpa

--- src/main/java/io/springbootlab/springbootsecuritymysql/SpringbootSecurityMysqlApplication.java
+++ src/main/java/io/springbootlab/springbootsecuritymysql/SpringbootSecurityMysqlApplication.java
@@ -2,8 +2,10 @@ package io.springbootlab.springbootsecuritymysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("io.springbootlab.springbootsecuritymysql.*")
 public class SpringbootSecurityMysqlApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/io/springbootlab/springbootsecuritymysql/controller/HomePageController.java
@@ -0,0 +1,44 @@
+package io.springbootlab.springbootsecuritymysql.controller;
+
+/*
+        CREATE TABLE user (id INT(20), active VARCHAR(20),
+        roles VARCHAR(20), password VARCHAR(20), user_name VARCHAR(20));
+
+        INSERT INTO user(id,active,roles,password,user_name)
+        values (1,'true','ROLE_USER','pass123','user');
+*/
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/home")
+public class HomePageController {
+
+    @RequestMapping(method = RequestMethod.GET,value = "/")
+    public String returnHomePageGeneric(){
+        return "Welcome to common Env\n"+new Date();
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/test")
+    public String returnHomePageTester(){
+        return "Welcome to Testing Env\n"+new Date();
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/dev")
+    public String returnHomePageDeveloper(){
+        return "Welcome to Developing Env\n"+new Date();
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/prod")
+    public String returnHomePageAdmin(){
+        return "Welcome to Production Env\n"+new Date();
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/user")
+    public String returnHomePageUser(){
+        return "Welcome to User Env\n"+new Date();
+    }
+}
